b6ade7831aef28f2e181dc76afd0dde895fbac2a
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-15 18:39:01 -0400

    fix production paths

--- knexfile.example.js
+++ knexfile.example.js
@@ -44,10 +44,10 @@ module.exports = {
     client: 'pg',
     connection: productionDbConnection,
     migrations: {
-      directory: './data/migrations'
+      directory: './db/migrate'
     },
     seeds: {
-      directory: './data/seeds'
+      directory: './db/seeds'
     },
     ssl: true
   }
