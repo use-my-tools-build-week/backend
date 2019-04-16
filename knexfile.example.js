fc877866fd79c579df488362be81eee39a49b67f
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-04-16 14:30:16 -0400

    gitignore knexfile.local.js
    made this change to allow for a local knexfile

--- knexfile.example.js
+++ knexfile.example.js
@@ -6,50 +6,50 @@ const localPg = {
   database: 'use_my_tools',
   user: 'umt_user',
   password: 'umt_password'
-}
-
-const productionDbConnection = process.env.DATABASE_URL || localPg
-
-module.exports = process.env.KNEXFILE_PATH || {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './db/dev.sqlite3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './db/migrate'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  },
-
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './db/test.sqlite3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './db/migrate'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: productionDbConnection,
-    migrations: {
-      directory: './db/migrate'
-    },
-    seeds: {
-      directory: './db/seeds'
-    },
-    ssl: true
-  }
-
 };
+
+const productionDbConnection = process.env.DATABASE_URL || localPg;
+
+module.exports = process.env.KNEXFILE_PATH
+  ? require(process.env.KNEXFILE_PATH)
+  : {
+      development: {
+        client: 'sqlite3',
+        connection: {
+          filename: './db/dev.sqlite3'
+        },
+        useNullAsDefault: true,
+        migrations: {
+          directory: './db/migrate'
+        },
+        seeds: {
+          directory: './db/seeds'
+        }
+      },
+
+      testing: {
+        client: 'sqlite3',
+        connection: {
+          filename: './db/test.sqlite3'
+        },
+        useNullAsDefault: true,
+        migrations: {
+          directory: './db/migrate'
+        },
+        seeds: {
+          directory: './db/seeds'
+        }
+      },
+
+      production: {
+        client: 'pg',
+        connection: productionDbConnection,
+        migrations: {
+          directory: './db/migrate'
+        },
+        seeds: {
+          directory: './db/seeds'
+        },
+        ssl: true
+      }
+    };
